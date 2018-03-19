c6f2c134bd43d7752ca902afffc66eb76a4f5bd8
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-19 17:36:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ var height = "74"
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(height);
+
+console.log(parseInt(height) + 12);
+
